088e7df96ac1a33395991a62126befa7518ebad0
nzhek <nzhek@mail.ru>
2016-04-11 11:02:15 +0300

    Cors filter

---
+++ src/main/java/com/devcore/filter/CORSFilter.java
@@ -0,0 +1,30 @@
+package com.devcore.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        resp.setHeader("Access-Control-Max-Age", "3600");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+        chain.doFilter(request, resp);
+    }
+
+    @Override
+    public void destroy() {}
+
+}
