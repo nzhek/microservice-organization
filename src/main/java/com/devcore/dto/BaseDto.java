bebfd969f1bf462f3d1e3819ffda745e5848b479
nzhek <nzhek@mail.ru>
2016-04-12 16:48:16 +0300

    create baseentity and extends hashcode and equals and primary id for entity

---
+++ src/main/java/com/devcore/dto/BaseDto.java
@@ -0,0 +1,18 @@
+package com.devcore.dto;
+
+/**
+ * base dto
+ * Created by e.novichenko on 12.04.2016.
+ */
+public abstract class BaseDto {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/devcore/entity/BaseEntity.java
@@ -0,0 +1,51 @@
+package com.devcore.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.search.annotations.DocumentId;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Base entity
+ */
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @DocumentId
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof BaseEntity)) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return new EqualsBuilder()
+                .append(getId(), that.getId())
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(getId())
+                .toHashCode();
+    }
+
+}
